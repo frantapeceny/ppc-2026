d0e25daf4b209d542a1e49f68d871bb7bf7ecd73
pecenyfrantisek <pecenyfrantisek5@gmail.com>
2026-02-24 11:50:53 +0100

    snaha o parsing do structu

--- ukol-1/ukol-1.cpp
+++ ukol-1/ukol-1.cpp
@@ -1,29 +1,43 @@
 
 #include <iostream>
 #include <string>
-
-
+#include <vector>
 
 using namespace std;
 
-
+struct konfigurace{
+    string funkce;
+    string hodnota;
+};
 
 int main(){
-    string function, configFormalita, value;
+    string funkce, formalita, hodnota;
+    vector <konfigurace> poleKonfiguraci;
+    int i = 0;
 
     cout << "zkouska" << endl;
 
     while (cin.peek() != '='){
-        getline(cin, configFormalita,'.');
-        getline(cin, function, '=');
-        getline(cin, value);
-        cout << configFormalita << "\n" << function << "\n" << value << endl;
+        getline(cin, formalita,'.');
+        getline(cin, funkce, '=');
+        getline(cin, hodnota);
 
+        konfigurace currentConfig;
+        
+        currentConfig.funkce = funkce;
+        currentConfig.hodnota = hodnota;
+
+        poleKonfiguraci.push_back(currentConfig);
+
+        //cout << formalita << "\n" << funkce << "\n" << hodnota << endl;
+    }
+
+    for (int i = 0; i < poleKonfiguraci.size(); i++){
+        cout << poleKonfiguraci[i].funkce << " " << poleKonfiguraci[i].hodnota << endl;
     }
 
     // line.find(), line.substr(), line.erase(), line.replace(), line.insert(), line.append(), line.compare(), line.find_first_of(), line.find_last_of(), line.find_first_not_of(), line.find_last_not_of()
 
-    cout << value << "\n" << function << "\n" << configFormalita << endl;
     return 0;
 
 }
