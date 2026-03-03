86aad3bb7aa34112f9409093c88f09184a064b8b
pecenyfrantisek <pecenyfrantisek5@gmail.com>
2026-03-03 11:56:07 +0100

    dodelana funkce loadNums

--- ukol-1/ukol-1.cpp
+++ ukol-1/ukol-1.cpp
@@ -33,21 +33,27 @@ void loadConfig(){
     cin.ignore(); // preskocim "="
     cin.ignore(); // preskocim newline
 
-    for (int i = 0; i < poleKonfiguraci.size(); i++){
+    if (poleKonfiguraci.size() == 4){
+        for (int i = 0; i < poleKonfiguraci.size(); i++){
         cout << poleKonfiguraci[i].funkce << " " << poleKonfiguraci[i].hodnota << endl;
     }
+    }
 
-    cout << "checkpoint - zpracovany config" << endl;
+    for (int i = 0; i < poleKonfiguraci.size(); i++){
+        if poleKonfiguraci[i]
+    }
     
-    //cout << "pocet ulozenych configu: " << poleKonfiguraci.size() << endl;
+    
+
 }
 
 void loadNums(){
     string cislo, radek;
-    vector <string> poleCisel;
+    vector <int> poleCisel;
     vector <int> pocetCisel;
-    int maxPocetCiselNaRadku = 0;
+    int maxDelka = 0;
     int pocetCiselNaRadku = 0;
+    int sum = 0;
 
     while (cin.peek() != EOF){
         getline(cin, radek, '\n');   
@@ -58,47 +64,58 @@ void loadNums(){
         while(radekSS.peek() != EOF){
             getline(radekSS, cislo, ';');
 
-            poleCisel.push_back(cislo);
-            pocetCiselNaRadku ++;
+            if (cislo.at(0) == 'S'){
+                char zacatekIntervalu = cislo.at(4);
+                char konecIntervalu = cislo.at(6);
 
-            /*
-            if (poziceDvojtecky = cislo.find(':')){ //nemusi fungovat
-                prvniPoziceProSum = cislo[poziceDvojtecky-1] - 'A' + 1; // nebo -65
-                druhaPoziceProSum = cislo[poziceDvojtecky+1] - 'A' + 1;
-                //druhaPoziceProSum - prvniPoziceProSum;
-                cout << prvniPoziceProSum << " " << druhaPoziceProSum << endl;
+                // convertuji pismeno na pozici
+                int prvniNum = zacatekIntervalu - 'A';
+                int druhyNum = konecIntervalu - 'A';
 
+                for (int i = prvniNum; i <= druhyNum; i++){                
+                    sum = sum + poleCisel[i];
+                }
+                poleCisel.push_back(sum);
             } else {
-                poleCisel.push_back(cislo);
-            }*/
+                poleCisel.push_back(stoi(cislo));
+            }
+
+            pocetCiselNaRadku ++;
         }
 
         pocetCisel.push_back(pocetCiselNaRadku);
 
-        if (pocetCiselNaRadku > maxPocetCiselNaRadku){
-            maxPocetCiselNaRadku = pocetCiselNaRadku;
+        if (pocetCiselNaRadku > maxDelka){
+            maxDelka = pocetCiselNaRadku;
         }
+        pocetCiselNaRadku = 0;
     }
-
-    cout << "extraktovana cisla: " << endl;
-    cout<< "pocetcisel.size: " << pocetCisel.size() << endl;
 
     int pocetRadku = pocetCisel.size();
 
+    cout << "pocet cisel na radek: ";
+
     for (int h = 0; h < pocetRadku; h++){
-        cout <<"radek cislo " << h << ": " << endl;
-        for (int i = 0; i < pocetCisel.at(h); i++){
-            cout << poleCisel[i] << endl;
-        }        
+        cout << pocetCisel[h] << ", ";
     }
+
+    cout << "max pocet cisel na radek: " << maxDelka << endl;    
+
+    // cisla jsou ulozena v poli poleCisel, delky radku jsou v poli pocetCisel, pocet radku je v hodnota promenne pocetRadku, maximalni delka radku je v promenne maxDelka
 }
+
+
 
 int main(){
     loadConfig();
-
     loadNums();
+
+
 
     return 0;
 }    
 
 // line.find(), line.substr(), line.erase(), line.replace(), line.insert(), line.append(), line.compare(), line.find_first_of(), line.find_last_of(), line.find_first_not_of(), line.find_last_not_of()
+
+
+// zeptat na this - co ze to nahrazuje
